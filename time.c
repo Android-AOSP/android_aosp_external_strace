c1e9b247c5b5a6c45ed0aca38113fb85d2525d3b
Mike Frysinger <vapier@gentoo.org>
2014-12-30 19:08:50 -0500

    timerfd: update flags decoding
    The current code only decodes TFD_TIMER_ABSTIME when the kernel has
    a bunch of newer flags.  We have to handle fallbacks for all of them
    since the kernel doesn't currently export things to userspace.
    
    * time.c: Include fcntl.h for O_* defines.
    (TFD_TIMER_ABSTIME): Move to xlat/timerfdflags.in.
    * xlat/timerfdflags.in: Add more definitions.

--- time.c
+++ time.c
@@ -28,6 +28,7 @@
  */
 
 #include "defs.h"
+#include <fcntl.h>
 #include <linux/version.h>
 #include <sys/timex.h>
 #include <linux/ioctl.h>
@@ -823,10 +824,6 @@ rtc_ioctl(struct tcb *tcp, long code, long arg)
 	}
 	return 1;
 }
-
-#ifndef TFD_TIMER_ABSTIME
-#define TFD_TIMER_ABSTIME (1 << 0)
-#endif
 
 #include "xlat/timerfdflags.h"
 
